f322bf58344ff1cd906b41a466d052989bddea82
pixiee534 <pixiee345@gmail.com>
2024-09-16 12:06:14 +0530

    homework

---
+++ homework.js
@@ -0,0 +1,8 @@
+var student = {
+    name: 'Akshyani', 
+    age: '13',
+    Grade: '8',
+    hobbies: 'Singing',
+    fav_sport: 'Football' };
+var color=["white", "black", "red", "green", "blue"];
+console.log(student.name)
